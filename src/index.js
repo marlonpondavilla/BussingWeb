0c2a28888147426d851df753a8d3d587f57cef7e
MarlonDev <marlonpondavilla.com>
2024-12-06 10:43:03 +0800

    initial commit v1.0.1

---
+++ src/index.js
@@ -0,0 +1,34 @@
+    import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js';
+    import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js';
+    import { getAuth, signInWithPopup, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js';
+    import { firebaseConfig } from '../scripts/firebaseConfig.js';
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    const auth = getAuth();
+    const googleProvider = new GoogleAuthProvider();
+
+    // Google Sign-In button event listener
+    const googleBtn = document.getElementById('google-login-btn');
+    const userName = document.getElementById('user-name');
+
+    googleBtn.addEventListener('click', () => {
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {          
+                window.location.href = '/pages/landingPage.html';
+            })
+            .catch((error) => {
+                console.error('Error during sign-in: ', error.message);
+                alert('Error: ' + error.message);
+            });
+    });
+
+    // Check if user is signed in
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+            userName.textContent = user.displayName;
+        } else {
+            console.log('User is not signed in');
+        }
+    });
